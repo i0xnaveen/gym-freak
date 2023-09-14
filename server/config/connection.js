a7c9ce4b9a8e78dd0526e24744506003761d5d35
i0xnaveen <124484861+i0xnaveen@users.noreply.github.com>
2023-09-15 00:37:42 +0530

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.URI, {
+mongoose.connect('mongodb+srv://naveen62113:Ganapathy123@cluster0.1c0x0dx.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
